2f7e551ee4d70698ebff055471e4150a208e1eed
Mikael <anz.mikail@mail.ru>
2025-05-23 23:15:08 +0300

    test

--- src/Components/Main/ContactMe/ContactMe.jsx
+++ src/Components/Main/ContactMe/ContactMe.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { BiLogoGithub, BiLogoTelegram,  BiLogoVk, BiLogoWhatsapp } from "react-icons/bi";
+import { BiLogoGithub, BiLogoTelegram, BiLogoWhatsapp } from "react-icons/bi";
 import './ContactMe.css'
 import {useSelector} from "react-redux";
 
 
 function ContactMe () {
-    const language = useSelector(state => state.Data.language)
     const theme = useSelector(state => state.Data.theme)
     return (
         <div className={theme? "contactMe": "contactMe white" }>
             <h2>Контакты</h2>
             <div>
                 <ul className="self_info">
-                    <li>Телефон: 8(938)024-04-66 <BiLogoWhatsapp /></li>
+                    <li>Телефон: 8(938)024-04-66 <BiLogoWhatsapp/></li>
                     <li>e-mail: anz.mikail@mail.ru</li>
                     <li>GitHub: <a href='https://github.com/anz-mikail'><BiLogoGithub/></a></li>
                     <li>Telegram: <a href='https://t.me/Mikael_Anz'><BiLogoTelegram/></a></li>
