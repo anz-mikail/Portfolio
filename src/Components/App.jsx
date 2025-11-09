84876c74e2e4d0b0ad79fc20affa2f3507dc747a
Mikael <anz.mikail@mail.ru>
2025-11-09 15:06:00 +0300

    test

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -28,7 +28,7 @@ function App() {
                 onClick={handleChangeMenuActive}
                 className={theme ? "body" : 'body white'}>
                 <Routes>
-                    <Route path='/' element={<Main/>}/>
+                    <Route path='/portfolio' element={<Main/>}/>
                     <Route path='/silant' element={<Silant/>}/>
                     <Route path='/scan' element={<Scan/>}/>
                     <Route path='/currency' element={<Currency/>}/>
